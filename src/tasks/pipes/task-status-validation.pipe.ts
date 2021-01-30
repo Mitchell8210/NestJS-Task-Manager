978226da9d10d3c240391b8270a732afc9fdb627
Mitchell Rhoads <mrhoads@tunego.com>
2021-01-29 16:17:54 -0800

    Initial commit, New Tasks Module with corresponding files for dtos, services, controllers, and models

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,18 @@
+import {  BadRequestException, PipeTransform } from "@nestjs/common";
+import { TaskStatus } from "../task.model";
+
+export class TaskStatusValidationPipe implements PipeTransform {
+    readonly allowedStatuses = [
+        TaskStatus.OPEN,
+        TaskStatus.IN_PROGRESS,
+        TaskStatus.DONE,
+    ]
+
+    transform(value: any) {
+        value = value.toUpperCase();
+        if(!this.allowedStatuses.includes(value)) {
+            throw new BadRequestException(`${value} is an invalid status`)
+        }
+        return value
+    }
+}
